0ba5b91f6bb35110ac36ece51848ff3919b27a05
Your Name <honeytalluri99@gmail.com>
2023-04-22 17:28:07 -0500

    3rd commit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request ,redirect , url_for ,session
 import pymysql
 import re
 import os
@@ -37,16 +37,24 @@ def home():
 @app.route('/home',methods=['GET','POST'])
 def main():
     return render_template('Home.html')
+@app.route('/product',methods=['GET','POST'])
+def product():
+    conn, c = sql_connector()
+    c.execute('SELECT * FROM product')
+    fetchdata = c.fetchall()
+    return render_template('product.html',products=fetchdata)
+@app.route('/about',methods=['GET'])
+def about():
+    return render_template('about.html')
 @app.route('/payment',methods=['GET','POST'])
 def payment():
-    return render_template('payment.hml')
+    return render_template('payment.html')
 @app.route('/create',methods=['GET','POST'])
-@app.route('/login/register', methods=['GET', 'POST'])
 def register():
     # Output message if something goes wrong...
     msg = ''
     # Check if "username", "password" and "email" POST requests exist (user submitted form)
-    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
+    if request.method == 'POST' and 'ssn' in request.form and 'password' in request.form and 'fname' in request.form:
         # Create variables for easy access
         fname = request.form['fname']
         lname = request.form['lname']
@@ -56,22 +64,25 @@ def register():
         dob = request.form['dob']
         gender = request.form['gender']
         password = request.form['password']
+        print(fname)
+
 
         # Check if account exists using MySQL
         conn, c = sql_connector()
-        c.execute('SELECT * FROM person WHERE SSN = %s', (ssn))
-        account = c.fetchone()
+        c.execute('INSERT INTO person VALUES(%s, %s, %s ,%s ,%s,%s,%s)',(ssn, fname, lname ,gender, phonenumber, dob,password))
+        conn.commit()
         # If account exists show error and validation checks
-        if account:
-            msg = 'Account already exists!'
-        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
-            msg = 'Invalid email address!'
-        elif not re.match(r'[A-Za-z0-9]+', username):
-            msg = 'Username must contain only characters and numbers!'
+        if c.rowcount==1:
+            msg = 'Registerd Succesfully '
+            return redirect(url_for('home'))
+        # elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+        #     msg = 'Invalid email address!'
+        # elif not re.match(r'[A-Za-z0-9]+', username):
+        #     msg = 'Username must contain only characters and numbers!'
         # elif not username or not password or not email:
         #     msg = 'Please fill out the form!'
-    elif request.method == 'POST':
-        msg = 'Please fill out the form!'
+    # elif request.method == 'POST':
+    #     msg = 'Please fill out the form!'
 
     return render_template('create.html', msg=msg)
 if __name__ == '__main__':
